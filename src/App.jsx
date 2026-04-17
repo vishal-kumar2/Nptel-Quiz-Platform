781f179955d33905797df4ff252ac3a7123732f2
vishal-kumar2 <vishalk73877@gmail.com>
2026-04-17 22:52:59 +0530

    added vercel package in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
 import useAuth from "./hooks/useAuth";
