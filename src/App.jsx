fedaaedaa187dc5e1e3fe7f4ecee0ee0ea0ddc3e
vishal-kumar2 <vishalk73877@gmail.com>
2026-04-17 22:45:16 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Analytics } from "@vercel/analytics/next"
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
 import useAuth from "./hooks/useAuth";
